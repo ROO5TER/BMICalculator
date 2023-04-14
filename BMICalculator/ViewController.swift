14bc2d8838c2a6d162e8544160cbf449d2ea0e8f
RafalSwat <rafal.swat@gmail.com>
2023-04-14 14:13:36 +0200

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Łukasz Skopiński on 14/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
